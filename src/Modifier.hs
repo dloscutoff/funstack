f8cd57a3d4419166e111f02d3d045815a012618c
David Loscutoff <dloscutoff@gmail.com>
2024-06-01 16:47:59 -0500

    Added Stack functions pop and popN.

--- src/Modifier.hs
+++ src/Modifier.hs
@@ -4,7 +4,7 @@ module Modifier (
 ) where
 
 import Function (Function)
-import State (Stack)
+import State (Stack, popN)
 
 -- A Modifier transforms one or more Functions into a new Function
 data Modifier =
@@ -13,15 +13,21 @@ data Modifier =
   Modifier3 (Function -> Function -> Function -> Function) |
   Modifier4 (Function -> Function -> Function -> Function -> Function)
 
+-- Modifiers are not normally shown, but can be in some error messages
+instance Show Modifier where
+  show (Modifier1 _) = "<1-modifier>"
+  show (Modifier2 _) = "<2-modifier>"
+  show (Modifier3 _) = "<3-modifier>"
+  show (Modifier4 _) = "<4-modifier>"
+
 -- Apply a Modifer to a Stack, resulting in a new Stack
 -- For an n-modifier, pop n Functions from the Stack, apply the modifier
--- function to them in the reverse of the order in which they were popped,
--- and push the single resulting Function back onto the Stack
--- If there are not enough Functions on the Stack to apply the Modifier,
--- push copies of the identity Function until there are enough
+-- function to them, and push the single resulting Function back onto
+-- the Stack
 modify :: Stack -> Modifier -> Stack
-modify (f : fs) (Modifier1 m) = m f : fs
-modify (g : f : fs) (Modifier2 m) = m f g : fs
-modify (h : g : f : fs) (Modifier3 m) = m f g h : fs
-modify (i : h : g : f : fs) (Modifier4 m) = m f g h i : fs
-modify fs m = modify (mempty : fs) m
+modify s m
+  | (Modifier1 mfn) <- m, ([f], s') <- popN 1 s = mfn f : s'
+  | (Modifier2 mfn) <- m, ([f, g], s') <- popN 2 s = mfn f g : s'
+  | (Modifier3 mfn) <- m, ([f, g, h], s') <- popN 3 s = mfn f g h : s'
+  | (Modifier4 mfn) <- m, ([f, g, h, i], s') <- popN 4 s = mfn f g h i : s'
+  | otherwise = error $ "Error while applying " ++ show m ++ " to stack " ++ show s
--- src/State.hs
+++ src/State.hs
@@ -1,7 +1,9 @@
 module State (
   Stack,
   State (..),
-  emptyState
+  emptyState,
+  pop,
+  popN
 ) where
 
 import Value (Value)
@@ -20,3 +22,20 @@ data State = State {
 -- Create a new State with the given arguments and an empty stack
 emptyState :: [Value] -> State
 emptyState args = State [] args
+
+-- Pop the top Function from a Stack, returning the Function and a
+-- new Stack with it missing
+-- If the Stack is empty, use the identity function
+pop :: Stack -> (Function, Stack)
+pop (f : fs) = (f, fs)
+pop [] = (mempty, [])
+
+-- Pop n Functions from a Stack, returning a list of Functions and a
+-- new Stack with them missing
+-- The Functions are listed with the top of the Stack at the end of
+-- the list, in keeping with the order in which they were (probably)
+-- pushed: a program like A B over should give A over B, not B over A
+-- If the Stack does not contain enough elements, fill in with the
+-- identity function
+popN :: Int -> Stack -> ([Function], Stack)
+popN n s = (reverse $ take n $ s ++ cycle mempty, drop n s)
